00bdedcdac8200a7654563c895291a332cc5b3a8
Nicolai Berthelsen <Nicolai.Berthelsen@komplett.com>
2019-04-17 20:37:30 +0200

    use app api instead of parsing html

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # mykid-backend
+
+# Create and activate virtualenv
+.\virtualenv\Scripts\activate
+
+# Run flask
+set FLASK_APP=mykid.py
+flask run
--- mykid.py
+++ mykid.py
@@ -1,22 +1,55 @@
 import requests
 from flask import Flask, request, jsonify
+from myhtmlparser import MyHTMLParser
+import urllib.parse
+import json
 
 app = Flask(__name__)
 
 class User(object):
     def __init__(self, app):
         self.app = app
-        self.session = None
+        self.token = None
+        self.details = {}
+        self.children = []
 
-    def set_session(self, session):
-        self.session = session
+    def set_token(self, token):
+        self.token = token
 
-    def get_session(self):
-        return self.session
+    def get_token(self):
+        return self.token
+
+    def set_details(self, details):
+        self.details = details
+
+    def get_details(self):
+        return self.details
+
+    def set_children(self, data):
+        self.children = parse_children(data)
+
+    def get_children(self):
+        return self.children
+
+    def parse_children(self, data):
+        children = []
+        for child in data:
+            children.append({
+                "avdeling_navn": child["avdeling"]["navn"],
+                "avdeling_nummer": child["avdeling"]["telefon"],
+                "bursdag": child["birthday"],
+                "id": child["id"],
+                "img": child["img_src"],
+                "navn": "{} {}".format(child["fornavn"], child["etternavn"])
+            })
+        return children
+
 
 user = User(app)
-login_url = "https://mykid.no/forside/forside/my_brukere_logginn"
-show_day_url = "https://mykid.no/_ajax/dagenmin/show_myday"
+LOGIN_API_URL = "https://m.mykid.no/api/authenticate"
+GET_KIDS_API_URL = "https://m.mykid.no/api/dashboard/get_kids"
+SHOW_DAY_API_URL = "https://m.mykid.no/api/week_events/day_data/110916/"
+COOKIE = "mykid"
 
 @app.route('/ping')
 def ping():
@@ -24,24 +57,51 @@ def ping():
 
 @app.route('/api/login', methods=['POST'])
 def login():
-    if (user.get_session() != None):
+    if (user.get_token() == None):
         login_data = request.get_json()
-        s = requests.session()
-        user.set_session(s)
-        s.post(login_url, login_data)
+        headers = {
+            "Accept": "application/json",
+            "Content-Type": "application/json;charset=UTF-8"
+        }
+        res = requests.post(LOGIN_API_URL, json=login_data, headers=headers)
+        data = res.json()
+        print(res.json())
+        user_data = data["user"]
+        user.set_details(user_data)
+        user.set_token(user_data.get("token", None))
+    else:
+        print("Already logged in")
 
     return jsonify({
-        "message": "Logget inn"
+        "user": user.get_details()
     })
+
+@app.route("/api/children", methods=['GET'])
+def children():
+    children = user.get_children()
+    if (len(children) != 0):
+        cookie = request.cookies.get(COOKIE, {})
+        if (cookie):
+            decoded_cookie = urllib.parse.unquote(cookie)
+            user = json.loads(decoded_cookie)
+            token = user["token"]
+            headers = {
+                "X-Auth-Token": token
+            }
+            res = requests.get(GET_KIDS_API_URL, headers=headers)
+            return jsonify(res.json())
+        else:
+            return {}
+    else:
+        return jsonify(children)
 
 @app.route("/api/my_day")
 def my_day():
-    data = {
-        "date": "2019-03-21 00:00:00"
-    }
-    print("after")
-    s = user.get_session()
-    print(s)
-    res = s.post(show_day_url, data)
-    print(res.text)
-    return res.text
+    # s = user.get_session()
+    # date = "2019-03-19"
+    # res = s.get(show_day_url + date)
+    # print(res.text)
+    # parser = MyHTMLParser(res.text)
+    # data = parser.get_day()
+    data = {}
+    return jsonify(data)

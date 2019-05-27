0a7af9eb24f07959fb4f1475e360d3a82c128d01
Nicolai Berthelsen <Nicolai.Berthelsen@komplett.com>
2019-05-27 11:16:56 +0200

    allow for more users

--- mykid.py
+++ mykid.py
@@ -48,7 +48,7 @@ class User(object):
     def get_plannings(self):
         return self.plannings
 
-user = User(app)
+users = {}
 LOGIN_API_URL = "https://m.mykid.no/api/authenticate"
 GET_KIDS_API_URL = "https://m.mykid.no/api/dashboard/get_kids"
 SHOW_DAY_API_URL = "https://m.mykid.no/api/week_events/day_data"
@@ -61,7 +61,10 @@ def ping():
 
 @app.route('/api/login', methods=['POST'])
 def login():
-    if (user.get_token() == None):
+    cookie = request.cookies.get(COOKIE, {})
+    user = get_user(cookie)
+    if not (user):
+        user = User(app)
         login_data = request.get_json()
         headers = {
             "Accept": "application/json",
@@ -69,22 +72,22 @@ def login():
         }
         res = requests.post(LOGIN_API_URL, json=login_data, headers=headers)
         data = res.json()
-        print(res.json())
         user_data = data["user"]
         user.set_details(user_data)
         user.set_token(user_data.get("token", None))
+        users[user.get_token()] = user
     else:
         print("Already logged in")
-
     return jsonify({
         "user": user.get_details()
     })
 
 @app.route("/api/children")
 def children():
+    cookie = request.cookies.get(COOKIE, {})
+    user = get_user(cookie)
     children = user.get_children()
     if (len(children) == 0):
-        cookie = request.cookies.get(COOKIE, {})
         if (cookie):
             headers = get_header_with_token(cookie)
             res = requests.get(GET_KIDS_API_URL, headers=headers)
@@ -96,8 +99,6 @@ def children():
 
 @app.route("/api/my_day")
 def my_day():
-    # date = datetime.date.today()
-    # date = "2019-04-25"
     url = "{}/{}/{}".format(SHOW_DAY_API_URL, request.args.get("child_id"), request.args.get("date"))
     cookie = request.cookies.get(COOKIE, {})
     if (cookie):
@@ -109,29 +110,36 @@ def my_day():
 
 @app.route("/api/plannings")
 def plannings():
-  plannings = user.get_plannings()
-  if len(plannings) == 0:
-      current_date = datetime.datetime.strptime(request.args.get("date"), "%Y-%m-%d")
-      cookie = request.cookies.get(COOKIE, {})
-      if (cookie):
-        headers = get_header_with_token(cookie)
-        for i in range(20): # look 20 weeks ahead
-          current_date = current_date + datetime.timedelta(days=7)
-          url = "{}/{}/{}".format(WEEK_EVENTS_API_URL, request.args.get("child_id"), str(current_date.date()))
-          res = requests.get(url, headers=headers)
-          days = res.json()
-          if len(days.get("days", [])) > 0:
-            for day in days["days"]:
-              events = day.get("events", [])
-              if len(events) > 0:
-                for event in events:
-                  if (event.get("navn", "").startswith("Planleggingsdag")):
-                    print("Found planning day")
-                    print(day["date"])
-                    plannings.append(day["date"])
-  return jsonify({
-    "planning_days": plannings
-  })
+    cookie = request.cookies.get(COOKIE, {})
+    user = get_user(cookie)
+    plannings = user.get_plannings()
+    if len(plannings) == 0:
+        current_date = datetime.datetime.strptime(request.args.get("date"), "%Y-%m-%d")
+        if (cookie):
+            headers = get_header_with_token(cookie)
+            for i in range(26): # look 26 weeks ahead
+                current_date = current_date + datetime.timedelta(days=7)
+                url = "{}/{}/{}".format(WEEK_EVENTS_API_URL, request.args.get("child_id"), str(current_date.date()))
+                res = requests.get(url, headers=headers)
+                days = res.json()
+                if len(days.get("days", [])) > 0:
+                    for day in days["days"]:
+                        events = day.get("events", [])
+                        if len(events) > 0:
+                            for event in events:
+                                if (event.get("navn", "").startswith("Planleggingsdag")):
+                                    plannings.append(day["date"])
+    return jsonify({
+        "planning_days": plannings
+    })
+
+def get_user(cookie):
+    if not (cookie):
+        return None
+    decoded_cookie = urllib.parse.unquote(cookie)
+    auth_user = json.loads(decoded_cookie)
+    token = auth_user["token"]
+    return users.get(token, None)
 
 def get_header_with_token(cookie):
     decoded_cookie = urllib.parse.unquote(cookie)
